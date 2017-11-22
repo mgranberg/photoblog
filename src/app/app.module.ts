7be36b0d475a39a67f23b7ea782b78ccaf94d7b5
mgranberg <mgranberg89@gmail.com>
2017-11-22 13:06:50 +0100

    update 05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent } from "./app.component"
-import { EntryListComponent, EntryComponent } from "./entries"
+import {HttpModule} from "@angular/http";
+import { AppComponent } from "./app.component";
+import { EntryListComponent, EntryComponent, EntryService } from './entries';
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        HttpModule
+    ],
+    providers: [ ],
     declarations: [
         AppComponent,
         EntryComponent,
